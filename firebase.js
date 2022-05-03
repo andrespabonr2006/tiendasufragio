e098e9c0f02fd7ee81df2c7f10c3a99630588d88
rat <rat@gmail.com>
2022-05-03 15:51:12 -0500

    therat

---
+++ firebase.js
@@ -0,0 +1,19 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyA93O-dJ-lSCVZ2-a4lixdUqa2WGHb1VMw",
+    authDomain: "tiendita-27a12.firebaseapp.com",
+    projectId: "tiendita-27a12",
+    storageBucket: "tiendita-27a12.appspot.com",
+    messagingSenderId: "742697288535",
+    appId: "1:742697288535:web:c7bec7321770ba39733412"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+*/
