1d0d0d31c44082d2905fe8f37f8cca3c8bc5ed2c
conanguevon <conan@guevon.com>
2022-05-10 17:24:52 -0500

    a

--- controlador.js
+++ controlador.js
@@ -1,51 +1,51 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
+    nombre:"ark",
+    precio:40000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/ak.png?alt=media&token=40b2e0ac-ae15-47bd-ad04-7861ebe05e5b"
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
+    nombre:"bo3",
+    precio:100000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/bo3.png?alt=media&token=da388689-dfe4-4dcb-ab53-48e58b2e4890"
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
+    nombre:"budokai tenkaichi 3",
+    precio:50000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/bt3.png?alt=media&token=f7205bd5-0367-408b-a156-15829158c6ef"
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
+    nombre:"csgo",
+    precio:40000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/csgo.png?alt=media&token=acb9a229-7a8e-4a2a-8d56-cf2e4b2e8713"
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
+    nombre:"elden ring",
+    precio:200000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/er.png?alt=media&token=8680a5ff-0328-4a2e-b89b-9a625a29b021"
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
+    nombre:"fifa",
+    precio:200000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/fifa.png?alt=media&token=13a3dca0-7795-484d-ac41-b4c8c3aca9b1"
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
+    nombre:"dota 2",
+    precio:0,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/fifa.png?alt=media&token=13a3dca0-7795-484d-ac41-b4c8c3aca9b1"
 }
 
 let producto8={
