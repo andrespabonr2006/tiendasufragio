222092a64e2646f7acf244c05249a71bc2fad091
a <a@gmail.com>
2022-05-17 17:26:31 -0500

    cossi

--- controlador.js
+++ controlador.js
@@ -5,7 +5,7 @@ let producto1={
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/ak.png?alt=media&token=40b2e0ac-ae15-47bd-ad04-7861ebe05e5b",
     popularidad:"★★★★",
-    pais:   
+    pais:   ""
 }
 
 let producto2={
@@ -14,7 +14,7 @@ let producto2={
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/bo3.png?alt=media&token=da388689-dfe4-4dcb-ab53-48e58b2e4890",
     popularidad:"★★★★★",
-    pais:   
+    pais:   ""
 }
 
 let producto3={
@@ -23,7 +23,7 @@ let producto3={
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/bt3.png?alt=media&token=f7205bd5-0367-408b-a156-15829158c6ef",
     popularidad:"★★★★★",
-    pais:   
+    pais:  "" 
 }
 
 let producto4={
@@ -31,8 +31,8 @@ let producto4={
     precio:40000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/csgo.png?alt=media&token=acb9a229-7a8e-4a2a-8d56-cf2e4b2e8713",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto5={
@@ -40,8 +40,8 @@ let producto5={
     precio:200000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/er.png?alt=media&token=8680a5ff-0328-4a2e-b89b-9a625a29b021",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto6={
@@ -49,8 +49,8 @@ let producto6={
     precio:200000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/fifa.png?alt=media&token=13a3dca0-7795-484d-ac41-b4c8c3aca9b1",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:  "" 
 }
 
 let producto7={
@@ -58,8 +58,8 @@ let producto7={
     precio:0,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/foto%20dota%20.png?alt=media&token=5636bcf9-8075-486f-b793-82385192915b",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto8={
@@ -67,8 +67,8 @@ let producto8={
     precio:90000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/gta5.png?alt=media&token=86e35afa-4be3-43cf-a077-2b19b376cdd2",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto9={
@@ -76,8 +76,8 @@ let producto9={
     precio:100000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/halo.png?alt=media&token=6a199bb8-d60e-4841-b918-a3b5369096d1",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto10={
@@ -85,8 +85,8 @@ let producto10={
     precio:50000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/rl.png?alt=media&token=504fdf4a-d960-49a0-82e6-6673790f256a",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto11={
@@ -94,8 +94,8 @@ let producto11={
     precio:0,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/lol.png?alt=media&token=8de6d13d-fd44-4bfe-8cf5-70522fda7ab0",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto12={
@@ -103,8 +103,8 @@ let producto12={
     precio:45000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/lossims.png?alt=media&token=e4482c68-d224-4f3e-8b19-832437376964",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto13={
@@ -112,8 +112,8 @@ let producto13={
     precio:55000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/minecraft.png?alt=media&token=9230f3c5-8e1d-42e6-9fa3-2ddcdd931fc0",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto14={
@@ -121,8 +121,8 @@ let producto14={
     precio:65000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/mk.png?alt=media&token=ed89f3be-7716-4660-b949-2e0ced3e28b9",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 
 let producto15={
@@ -130,286 +130,288 @@ let producto15={
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto16={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto17={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto18={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto19={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto20={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto21={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto22={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto23={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto24={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto25={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto26={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto27={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto28={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto29={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto30={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto31={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto32={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto33={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto34={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto35={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto36={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto37={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto38={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto39={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto40={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto41={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto42={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto43={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto44={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto45={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto46={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:   ""
 }
 let producto47={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:  "" 
 }
 let producto48={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais: ""  
 }
 let producto49={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
     foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
-    popularidad:,
-    pais:   
+    popularidad:"★★★★★",
+    pais:  "" 
 }
 let producto50={
     nombre:"the witcher 3",
     precio:500000,
     estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendita-27a12.appspot.com/o/tw.png?alt=media&token=7b8b4b1d-4eba-44ef-bbe9-98d9982b649f",
+    popularidad:"★★★★★",
+    pais:  ""
 }
 
 //ARREGLO
@@ -463,7 +465,7 @@ let productos=Array(
     producto47,
     producto48,
     producto49,
-    producto50,    
+    producto50    
 )
 
 //RECORRO EL ARREGLO
@@ -492,10 +494,15 @@ productos.forEach(function(producto){
     estado.classList.add("text-center")
     estado.textContent=producto.estado
 
+    let popularidad=document.createElement("p")
+    popularidad.classList.add("text-center")
+    popularidad.textContent=producto.popularidad
+
     targeta.appendChild(foto)
     targeta.appendChild(nombreProducto )
     targeta.appendChild(precio)
     targeta.appendChild(estado)
+    targeta.appendChild(popularidad)
     columna.appendChild(targeta)
     contenedor.appendChild(columna)
 
